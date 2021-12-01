300ccfb9ac0e0c8683421bb7036974a151a68cb4
Marton Erdos <me412@cam.ac.uk>
2021-12-01 21:11:48 +0000

    Added the two repositories as submodules.

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -2,6 +2,22 @@
 
 echo "Cloning Minesweeper repositories"
 cd ..
-git clone https://bitbucket.org/me412/minesweeper-public.git
-git clone https://bitbucket.org/me412/jemalloc-msweeper-public.git
+
+if [ ! -f "minesweeper-public/.git" ]; then
+    echo 'MineSweeper submodule not found, calling "git submodule init"...'
+    cd minesweeper-public
+    git submodule init
+    cd ..
+fi
+
+if [ ! -f "jemalloc-msweeper-public/.git" ]; then
+    echo 'JeMalloc (msweeper) submodule not found, calling "git submodule init"...'
+    cd jemalloc-msweeper-public
+    git submodule init
+    cd ..
+fi
+
+echo "Done cloning/already cloned."
+
+cd scripts
 
