304fe21739ad4299c4a7b137e4bb8df19aebd8d3
Marton Erdos <me412@cl.cam.ac.uk>
2022-01-04 18:50:15 +0000

    Make clone.sh update submodules correctly
    If cloned without --recurse-submodules option, submodules were initialised,
    but not updated/fetched. This is now fixed.

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
 echo "===== Cloning Minesweeper repositories"
-cd ..
 
-if [ ! -f "minesweeper-public/.git" ]; then
-    echo 'MineSweeper submodule not found, calling "git submodule init"...'
-    cd minesweeper-public
-    git submodule init
-    cd ..
-fi
-
-if [ ! -f "jemalloc-msweeper-public/.git" ]; then
-    echo 'JeMalloc (msweeper) submodule not found, calling "git submodule init"...'
-    cd jemalloc-msweeper-public
-    git submodule init
-    cd ..
-fi
+git submodule init
+git submodule update
 
 echo "===== Done cloning/already cloned."
-
-cd scripts
 
