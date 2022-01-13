2579c49b492f0aea36f28ecacdb7298b5967d605
Marton Erdos <me412@cam.ac.uk>
2022-01-13 09:19:05 +0000

    Added a few more checks if compilation has succeeded.

--- scripts/build_minesweeper.sh
+++ scripts/build_minesweeper.sh
@@ -10,12 +10,25 @@ mkdir -p build
 make
 make install
 
-cp build/lib/libjemalloc.so ../lib/libjemalloc.so
+cp build/lib/libjemalloc.so ../lib/libjemalloc.so || { echo "JeMalloc failed to build!"; exit 1; }
 
 echo "===== Building MineSweeper"
 cd ../minesweeper-public
 ln -s ../jemalloc-msweeper-public/include/jemalloc/ jemalloc
-LDIR=$(pwd)/../lib make opt
+LDIR=$(pwd)/../lib make opt || { echo "MineSweeper failed to build!"; exit 1; }
 
+cd ../scripts
 echo "===== Done building MineSweeper & JeMalloc"
+
+if [ ! -f "../lib/libjemalloc.so" ];
+then
+    echo "lib/libjemalloc.so not found. Build failed?"
+    exit 1
+fi
+
+if [ ! -f "../lib/libminesweeper.so" ];
+then
+    echo "lib/libminesweeper.so not found. Build failed?"
+    exit 1
+fi
 
